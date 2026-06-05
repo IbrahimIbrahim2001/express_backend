7a6dc9fb4c6f6ef8aea017073f54c046d0299f4b
IbrahimIbrahim2001 <ii8538098@gmail.com>
2026-06-05 14:05:00 +0300

    added token and jwt logic

--- repositories/auth.repository.js
+++ repositories/auth.repository.js
@@ -1,6 +1,6 @@
 import Users from "../models/users.js"
 import bcrypt from "bcryptjs"
-
+import jwt from "jsonwebtoken";
 export const registerNewUser = async (data) => {
     const user = await Users.findOne({ email: data.email });
     if (user) {
@@ -9,11 +9,36 @@ export const registerNewUser = async (data) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(data.password, salt);
     const newUser = new Users({ ...data, password: hashedPassword });
-    await newUser.save();
-    return newUser;
+    const result = await newUser.save();
+    const { password, ...other } = result._doc;
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email,
+        name: user.name
+    }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+    return { other, token };
 }
 
 export const loginUser = async (data) => {
+    const user = await Users.findOne({ email: data.email });
+    if (!user) {
+        throw new Error("User not found");
+    }
+    const isPasswordMatch = await bcrypt.compare(data.password, user.password);
+    if (!isPasswordMatch) {
+        throw new Error("Invalid credentials");
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email,
+        name: user.name
+    }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+    return { user, token };
 }
 
 export const getUserData = (id) => {
